1f86787ff0002b730d4383682402b999c85678e7
Franco Gilli <francogilli10@gmail.com>
2023-11-27 09:32:05 -0300

    Primer commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+// src/Button.jsx
+import React from 'react';
+import '../App.css'; // Import your button CSS file
+
+function Button({ text, secondary, svgPath }) {
+  const buttonClass = secondary ? 'genButton secondary' : 'genButton';
+
+  return (
+    <button className={buttonClass}>
+      {svgPath && <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" className="sparkle">
+        <path d={svgPath}></path>
+      </svg>}
+      {text}
+    </button>
+  );
+}
+
+export default Button;

1001dd13e1d9613855f082cbfd93d101e5f63453
Franco Gilli <francogilli10@gmail.com>
2023-12-02 10:05:31 -0300

    feat: Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // App.jsx
 import React from 'react';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <>
+      
       <Hero />
       <Skills />
       <Projects />

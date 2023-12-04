147e495ad51a1ef952678f4abda6c2072ee77d52
Franco Gilli <francogilli10@gmail.com>
2023-12-04 09:14:08 -0300

    Added detection of active section in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { links } from '../lib/data';
 
@@ -6,32 +6,83 @@ const Header = () => {
   const [activeSection, setActiveSection] = useState(null);
   const [timeOfLastClick, setTimeOfLastClick] = useState(null);
   const [isNavVisible, setIsNavVisible] = useState(true);
-  
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      links.forEach((link) => {
+        const section = document.querySelector(link.hash);
+        if (section) {
+          const sectionTop = section.offsetTop;
+          const sectionHeight = section.offsetHeight;
+
+          if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            setActiveSection(link.name);
+          }
+        }
+      });
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   useEffect(() => {
     let timeoutId;
-
+  
     const handleMouseActivity = () => {
       if (!isNavVisible) {
         setIsNavVisible(true);
       }
-
+  
       if (timeoutId) {
         clearTimeout(timeoutId);
       }
-
+  
       timeoutId = setTimeout(() => {
         setIsNavVisible(false);
       }, 10000);
     };
-
+  
+    const handleScroll = () => {
+      requestAnimationFrame(() => {
+        const scrollPosition = window.scrollY;
+  
+        links.forEach((link) => {
+          const section = document.querySelector(link.hash);
+          if (section) {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+  
+            // Adjusted buffer to improve accuracy
+            const buffer = 400;
+  
+            if (
+              scrollPosition >= sectionTop - buffer &&
+              scrollPosition < sectionTop + sectionHeight - buffer
+            ) {
+              setActiveSection(link.name);
+            }
+          }
+        });
+      });
+    };
+  
     document.addEventListener("mousemove", handleMouseActivity);
-
+    document.addEventListener("scroll", handleScroll);
+  
     return () => {
       document.removeEventListener("mousemove", handleMouseActivity);
+      document.removeEventListener("scroll", handleScroll);
       clearTimeout(timeoutId);
     };
   }, [isNavVisible]);
+  
 
   return (
     <header className="z-[999] relative">
@@ -40,8 +91,7 @@ const Header = () => {
           <motion.div
             className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-b-3xl  border-white border-opacity-40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[28.8rem] sm:rounded-full bg-[#202022] border-black/40 bg-opacity-80"
             initial={{ y: -100, x: "-50%", opacity: 0 }}
-            animate={{ y: 0, x: "-50%", opacity: 1, 
-            transition: { duration: 0.5 } }}
+            animate={{ y: 0, x: "-50%", opacity: 1, transition: { duration: 0.5 } }}
             exit={{ y: -100, x: "-50%", opacity: 0 }}
           ></motion.div>
         )}
@@ -49,15 +99,17 @@ const Header = () => {
 
       <AnimatePresence>
         {isNavVisible && (
-          <nav className="flex fixed top-[0.15rem] left-1/2 h-16 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+          <nav
+            ref={navRef}
+            className="flex fixed top-[0.15rem] left-1/2 h-16 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+          >
             <ul className="flex gap-3 flex-nowrap sm:flex-nowrap w-full items-center justify-center gap-y-1 text-[1.1rem] sm:text-[0.9rem] md:text-[0.9rem] lg:text-[0.9rem] font-semibold text-gray-500 sm:w-[22rem] sm:gap-5">
               {links.map((link) => (
                 <motion.li
                   className="h-3/4 flex items-center justify-center relative "
                   key={link.hash}
                   initial={{ y: -100, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1, 
-                  transition: { duration: 0.5 } }}
+                  animate={{ y: 0, opacity: 1, transition: { duration: 0.5 } }}
                   exit={{ y: -100, opacity: 0 }}
                 >
                   <a
@@ -74,7 +126,7 @@ const Header = () => {
 
                     {link.name === activeSection && (
                       <motion.span
-                        className=" rounded-full absolute inset-0 -z-10 dark:bg-zinc-600 dark:bg-opacity-40"
+                        className="rounded-full absolute inset-0 -z-10 dark:bg-zinc-600 dark:bg-opacity-40"
                         layoutId="activeSection"
                         transition={{
                           type: "spring",

fc7d6409dc6d01de807c49fd1ea1838ecd8f347f
Franco Gilli <francogilli10@gmail.com>
2023-12-08 18:59:43 -0300

    Added styles to the message that indicates whether the Email was sent

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
     email: "",
     message: "",
   });
-
   const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState({ text: "", color: "" });
 
   const handleChange = (e) => {
     const { target } = e;
@@ -42,39 +42,40 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          setMessage({ text: "Thank you. I will get back to you as soon as possible.", color: "green" });
 
           setForm({
             name: "",
             email: "",
             message: "",
           });
+
+          // Clear the success message after 5 seconds
+          setTimeout(() => {
+            setMessage({ text: "", color: "" });
+          }, 5000);
         },
         (error) => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          setMessage({ text: "Ahh, something went wrong. Please try again.", color: "red" });
         }
       );
   };
 
   return (
-    <section id="contact" className="bg-[#14181a] rounded-3xl mb-7 mx-5 max-w-[40rem] lg:mx-auto scroll-mt-28 sm:mb-4 ">
-      <motion.div
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      >
+    <section id="contact" className="bg-[#14181a] rounded-3xl mb-7 mx-5 mt-32 max-w-[40rem] lg:mx-auto scroll-mt-28 sm:mb-4 ">
+      <motion.div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <div className="flex flex-col gap-4">
-          <h3 className="text-white text-center font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[40px]">Contact.</h3>
+          <h3 className="text-white text-center font-black md:text-[50px] sm:text-[50px] xs:text-[40px] text-[40px]">Contact.</h3>
 
-          <form 
-            ref={formRef}
-            onSubmit={handleSubmit} 
-            className="flex flex-col gap-6">
+          <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col gap-6">
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your Name</span>
-              <input type="text"
-                name='name'
+              <input
+                type="text"
+                name="name"
                 value={form.name}
                 onChange={handleChange}
                 placeholder="What's your name?"
@@ -83,8 +84,9 @@ const Contact = () => {
             </label>
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your email</span>
-              <input type="email"
-                name='email'
+              <input
+                type="email"
+                name="email"
                 value={form.email}
                 onChange={handleChange}
                 placeholder="What's your email address?"
@@ -95,7 +97,7 @@ const Contact = () => {
               <span className="text-white font-medium mb-4">Your Message</span>
               <textarea
                 rows={5}
-                name='message'
+                name="message"
                 value={form.message}
                 onChange={handleChange}
                 placeholder="What you want to say?"
@@ -112,6 +114,10 @@ const Contact = () => {
               </button>
             </div>
           </form>
+
+          {message.text && (
+            <div className={`text-${message.color}-500 font-semibold text-center mt-2`}>{message.text}</div>
+          )}
         </div>
       </motion.div>
     </section>

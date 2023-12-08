1434286f1ea5017fcaa7358d8dcb8550e20b99fc
Franco Gilli <francogilli10@gmail.com>
2023-12-08 15:35:42 -0300

    Adding small visual changes to Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,29 +3,7 @@ import { motion } from "framer-motion";
 
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
 
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-  };
 
   return (
     <section id="contact" className="bg-[#14181a] rounded-3xl mb-7 mx-5 max-w-[40rem] lg:mx-auto scroll-mt-28 sm:mb-4 ">
@@ -35,34 +13,27 @@ const Contact = () => {
         <div className="flex flex-col gap-4">
           <h3 className="text-white text-center font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[40px]">Contact.</h3>
 
-          <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col gap-6">
+          <form  className="flex flex-col gap-6">
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your Name</span>
-              <input type="text" name="name"
-                value={form.name}
-                onChange={handleChange}
+              <input type="text"
                 placeholder="What's your name?"
-                className="bg-zinc-800 py-3 px-6 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium"
+                className="bg-zinc-800 py-3 px-5 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium"
               />
             </label>
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your email</span>
-              <input type="email" name="email"
-                value={form.email}
-                onChange={handleChange}
+              <input type="email"
                 placeholder="What's your email address?"
-                className="bg-zinc-800 py-3 px-6 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium"
+                className="bg-zinc-800 py-3 px-5 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium"
               />
             </label>
             <label className="flex flex-col">
               <span className="text-white font-medium mb-4">Your Message</span>
               <textarea
-                rows={7}
-                name="message"
-                value={form.message}
-                onChange={handleChange}
+                rows={5}
                 placeholder="What you want to say?"
-                className="bg-zinc-800 py-3 px-6 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium "
+                className="bg-zinc-800 py-3 px-5 text-[.9375rem] placeholder:text-secondary text-white rounded-2xl outline-none border-none font-medium "
               />
             </label>
 
@@ -71,7 +42,7 @@ const Contact = () => {
                 type="submit"
                 className="bg-zinc-800 py-3 px-8 rounded-2xl outline-none w-fit text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-bold shadow-md shadow-primary"
               >
-                {loading ? "Sending..." : "Send"}
+                Send
               </button>
             </div>
           </form>

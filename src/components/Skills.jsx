e0aef652bb1709221b9480b0f0cd60f676ae5b10
Franco Gilli <francogilli10@gmail.com>
2023-12-28 17:14:24 -0300

    Added icons to the technology tags of the Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,29 +20,33 @@ const fadeInAnimationVariants = {
 
 function Skills({}) {
   return (
-    <section id="skills" className='mb-28 max-w-[53rem] mx-auto scroll-mt-28 text-center sm:mb-40 px-6'>
+    <section id="skills" className='mb-28 max-w-[53rem] mx-auto scroll-mt-28 text-center sm:mb-28 px-6'>
       <motion.h2 className='font-bold text-4xl text-white pb-10 items-center pt-10' variants={fadeInAnimationVariants}
             initial="initial"
             whileInView="animate"
             viewport={{
               once: true,
             }}>Skills</motion.h2>
-      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+      <ul className="flex flex-wrap justify-center gap-1 text-lg text-gray-800">
 
         {skillsData.map((skill, index) => (
-        <motion.li
-            className=" font-semibold text-base borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-            key={index}
-            variants={fadeInAnimationVariants}
+        <motion.span
+        key={index}
+        className={`text-white bg-white/5 border border-white/10 rounded-full inline-block mx-1 md:mx-1 py-1 px-2 md:py-2 md:px-4 text-xs md:text-sm transition hover:bg-white/10 mb-2`}
+        variants={fadeInAnimationVariants}
             initial="initial"
             whileInView="animate"
             viewport={{
               once: true,
             }}
-            custom={index}>
-          
-            {skill}
-          </motion.li>
+            custom={index}
+      >
+      {typeof skill === 'object' ? (
+        <span  className='flex items-center justify-center gap-1.5 cursor-pointer'>{skill.icon}{skill.name}</span>
+      ) : (
+        skill
+      )}
+          </motion.span>
         ))}
       </ul>
     </section>

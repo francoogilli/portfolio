1fb738c97dc358d40a417e604135f4fb45b58ffa
Franco Gilli <francogilli10@gmail.com>
2023-12-31 08:59:34 -0300

    Icons added to Read More and Show Less

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import { TAGS, PROJECTS } from "../lib/data"; // Actualiza la ruta de importación según tu estructura de archivos
 import SectionContainer from "./SectionContainer";
 import Brackets from '../icons/Brackets';
-import Chevrons from "../icons/Chevrons";
+import Chevrons from "../icons/ChevronsDown";
+import ChevronsUp from "../icons/ChevronsUp";
 import GitHubIcon from "../icons/GitHubIcon";
 import LiveProjectIcon from "../icons/LiveProjectIcon";
 const ProjectList = () => {
@@ -23,20 +24,21 @@ const ProjectList = () => {
             
             <h3 className="text-2xl font-semibold text-yellow-200 mb-2">{title}</h3>
             <div className={`text-lg text-white mb-0 text-pretty ${expandedProject === title ? '' : 'line-clamp-2'}`}>
-              {description.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                  <br />
-                </React.Fragment>
-              ))}
+            {description.split('\n').map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br />
+                <br />
+              </React.Fragment>
+            ))}
             </div>
             {description.length > 100 && (
               <button
                 onClick={() => toggleDescription(title)}
                 className="flex items-center gap-1 text-blue-500 cursor-pointer"
               >
-                {expandedProject === title ? 'Show Less' : 'Learn More'}<Chevrons/>
+                {expandedProject === title ? 'Show Less' : 'Read More'}
+                {expandedProject === title ? <ChevronsUp /> : <Chevrons />}
               </button>
             )}
             <ul className="flex gap-x-1 flex-row mb-3 pt-4">

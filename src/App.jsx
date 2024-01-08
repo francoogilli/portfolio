7f9d5d14f395cfad1f55ea598611386eda0abc2c
Franco Gilli <francogilli10@gmail.com>
2024-01-08 10:24:44 -0300

    Added a new redesign of the Hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Footer from './components/Footer';
+import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
@@ -15,7 +16,7 @@ function App() {
     <><div className="bg-[#1d1f31] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
     <div className="bg-[#181c26] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] "></div>
       <Navbar />
-      <Hero />
+      <Intro />
       <About />
       <Skills />
       <Projects />
---
+++ src/components/Intro.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import SectionContainer from "./SectionContainer";
+import foto from "../assets/img/midudev2.png";
+import LinkedIn from "../icons/LinkedIn";
+import Email from "../icons/Email";
+import Github from "../icons/Github";
+
+function Intro() {
+    return (
+        <main id="home" className="px-4 pb-8">
+            <SectionContainer className="py-44 pb-32 text-center flex flex-col items-center justify-center">
+                <img 
+                    src={foto} 
+                    alt="Franco Gilli photo" 
+                    className="rounded-full  w-24 h-24 mb-6 transition duration-700 ease-in-out hover:scale-110"
+                />
+                <h1 className="text-white text-4xl md:text-6xl lg:text-7xl font-bold flex flex-row gap-x-4 pb-6 lg:pb-10">
+                    Hey, I'm Franco
+                </h1>
+                <h2 class="text-xl lg:text-xl text-balance max-w-[700px] text-white">
+                <span>My name is <span className="font-medium ">Franco Gilli</span>, residing in </span><span
+                    class="text-red-200">
+                San Francisco, Córdoba, Argentina</span>
+                . <span class="text-yellow-200">
+                Full Stack developer</span>. <span class="text-sky-200"> 
+                Specialized in creating unique applications.</span>
+                </h2>
+                <nav class="flex gap-1 sm:gap-4 mt-8 flex-wrap">
+                    <a target="_blank" href="https://www.linkedin.com/in/franco-gilli/" className="bg-white/10 border border-white/10 rounded-full flex
+                         justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base 
+                         text-white/60 transition hover:scale-110 hover:bg-white/10 hover:text-white">
+                            <LinkedIn/>LinkedIn
+                    </a>
+                    <a target="_blank" href="https://github.com/francoogilli" className="bg-white/10 border border-white/10 rounded-full flex
+                         justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base 
+                         text-white/60 transition hover:scale-110 hover:bg-white/10 hover:text-white">
+                            <Github/>GitHub
+                    </a>
+                    <a target="_blank" href="mailto:francogilli10@gmail.com" className="bg-white/10 border border-white/10 rounded-full flex
+                         justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base 
+                         text-white/60 transition hover:scale-110 hover:bg-white/10 hover:text-white"><Email/>francogilli10@gmail.com
+                    </a>
+                </nav>
+            </SectionContainer>
+        </main>
+    );
+}
+
+export default Intro;

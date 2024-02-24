70420048f3fe66d3e183bf905d3dc87911172b6c
Franco Gilli <francogilli10@gmail.com>
2024-02-24 08:03:33 -0300

    Change border line color in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = ()=>{
     return(
 
-    <footer class="bg-[#070709] border-t border-gray-500">
+    <footer class="bg-[#070709] border-t border-zinc-900">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div class="flex justify-center sm:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 mx-auto">Build by <a href="mailto:francogilli10@gmail.com" class="font-bold text-slate-300">Franco</a></span>

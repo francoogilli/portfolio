35139015af677a4297a16e7befe4e1196ece6ed9
Franco Gilli <francogilli10@gmail.com>
2024-02-23 22:42:53 -0300

    Separate the about me text into more paragraphs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const fadeInAnimationVariants = {
     return(
         <section
             id="about" 
-            className="mb-28 max-w-[45rem] mx-auto text-center leading-9 px-6 sm:mb-40 scroll-mt-28"
+            className="mb-28 max-w-[45rem] mx-auto text-center leading-9 px-2 sm:mb-40 scroll-mt-28"
         >            
             <motion.h2 
                 variants={fadeInAnimationVariants}
@@ -38,7 +38,18 @@ const fadeInAnimationVariants = {
               once: true,
             }}
             className=" text-gray-300">
-                Después de completar la escuela Secundaria, decidí seguir mi pasión por la programación. Me inscribí en la carrera de <span className='font-semibold'>Técnico en Programación</span>, donde profundicé en el mundo del <span className="font-semibold">Desarrollo Web</span>. <span className="italic">Mi parte favorita de la programación</span> es el aspecto de resolución de problemas. Me encanta la sensación de finalmente encontrar una solución a un problema. Mi conjunto de tecnologías abarca tanto el <span className="font-medium">Frontend</span> como el <span className="font-medium">Backend</span>, y he aplicado el patrón de arquitectura MVC en .NET, integrando APIs externas y bases de datos.
+                Después de completar la escuela Secundaria, decidí seguir mi pasión por la programación. Me inscribí en la carrera de <span className='font-semibold'>Técnico en Programación</span>, donde profundicé en el mundo del <span className="font-semibold">Desarrollo Web</span>. 
+            </motion.p>
+            <motion.p
+             variants={fadeInAnimationVariants}
+             initial="initial"
+             whileInView="animate"
+             viewport={{
+               once: true,
+             }}
+             className='mt-3 text-gray-300'
+            >
+              <span className="italic">Mi parte favorita de la programación</span> es el aspecto de resolución de problemas. Me encanta la sensación de finalmente encontrar una solución a un problema. Mi conjunto de tecnologías abarca tanto el <span className="font-medium">Frontend</span> como el <span className="font-medium">Backend</span>, y he aplicado el patrón de arquitectura MVC en .NET, integrando APIs externas y bases de datos.
             </motion.p>
             <motion.p
              variants={fadeInAnimationVariants}
